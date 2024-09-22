10808789d3c08b650823d060eb6f1e34995e53a4
Muhammad Banhawy <devbenho@gmail.com>
2024-09-22 14:23:52 +0300

    ADDED: Entry endpoints that matches the routes to handlers -- SRV RUNNING

---
+++ user-service/cmd/user-service/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/devbenho/bazar-user-service/api/handlers"
+	"github.com/devbenho/bazar-user-service/api/middlewares"
+	configs "github.com/devbenho/bazar-user-service/configs"
+	"github.com/devbenho/bazar-user-service/internal/database"
+	"github.com/devbenho/bazar-user-service/internal/repositories"
+	"github.com/devbenho/bazar-user-service/internal/services"
+	"github.com/devbenho/bazar-user-service/pkg/tokens"
+	"github.com/devbenho/bazar-user-service/pkg/validation"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Load configuration
+	cfg, err := configs.LoadConfig()
+	if err != nil {
+		log.Fatalf("Error loading configuration: %v", err)
+	}
+
+	// Connect to the database
+	if err := database.Connect(); err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	defer func() {
+		if err := database.Disconnect(); err != nil {
+			log.Printf("Error disconnecting from the database: %v", err)
+		}
+	}()
+
+	// Initialize dependencies
+	validator := validation.NewValidator()
+	tokenService := tokens.NewTokenService(cfg.JWT.Secret)
+	userRepo := repositories.NewUserRepository(database.Database)
+	userService := services.NewUserService(validator, tokenService, userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
+	// Setup routes
+	mux := http.NewServeMux()
+	mux.HandleFunc("/user", userHandler.Register)
+	mux.HandleFunc("/user/login", userHandler.Login)
+	mux.HandleFunc("/user/get", userHandler.GetUserByID)
+	mux.HandleFunc("/user/update", userHandler.UpdateUser)
+	mux.HandleFunc("/user/delete", userHandler.DeleteUser)
+
+	// Wrap the mux with the error middleware
+	log.Printf("Starting server on port %s...", cfg.App.Port)
+	if err := http.ListenAndServe(":"+cfg.App.Port, middlewares.ErrorMiddleware(mux)); err != nil {
+		log.Fatal(err)
+	}
+}
